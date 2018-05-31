8481dca6fdb846ad5fb72829269ca6801e4f4563
iaortiz <38107740+iaortiz@users.noreply.github.com>
2018-05-31 11:48:43 -0500

    Agregando

--- Clase9/src/clase9/Ejercicio2.java
+++ Clase9/src/clase9/Ejercicio2.java
@@ -12,15 +12,15 @@ package clase9;
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        int[] elemento = {1,2,3,4,5,6,7};
-        int prod ;
-        for (int i = 0; i < elemento.length; i++) {
-            if (elemento[i] % 2 == 0) {
-                for (int j = 1; j < 13; j++) {
-                    prod = i * j;
-                    System.out.println(elemento[i]+ "x" + j+ " = " + prod);
+        int[] elemento = {1,2,3,4,5,6,7};//Declaracion de array
+        int prod ; //Declaracion de variable producto
+        for (int i = 0; i < elemento.length; i++) {// Ciclo for para recorrer el array
+            if (elemento[i] % 2 == 0) { // Condicional para los elementos pares
+                for (int j = 1; j < 13; j++) { //Array para los números a multiplicar
+                    prod = i * j; //Asignacion de valor para sacar producto
+                    System.out.println(elemento[i]+ "x" + j+ " = " + prod); //Presentacion
                 }
-                System.out.println("\n");
+                System.out.println("\n"); // Salto de linea por presentacion
             }
 
         }
