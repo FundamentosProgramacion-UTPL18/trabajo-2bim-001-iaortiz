5b61960dfbdfb3bc02ef3923855deeabab98fca9
iaortiz <38107740+iaortiz@users.noreply.github.com>
2018-05-31 11:44:47 -0500

    Update Ejercicio3.java

--- Clase9/src/clase9/Ejercicio3.java
+++ Clase9/src/clase9/Ejercicio3.java
@@ -16,17 +16,17 @@ public class Ejercicio3 {
         double[] calificacion_2 = {10, 12, 13, 20, 17, 20};// calificación 2
         double[] promedios = new double[6]; // Promedio del estudiante
 
-        for (int i = 1; i < promedios.length; i++) {
-            double suma = calificacion_1[i] + calificacion_2[i];
-            double promedio = suma / 2;
+        for (int i = 1; i < promedios.length; i++) { // Ciclo for para recorrer las posiciones
+            double suma = calificacion_1[i] + calificacion_2[i]; // Asignacion de valores para suma
+            double promedio = suma / 2; // Sacar promedio
 
-            promedios[i] = promedio;
+            promedios[i] = promedio; //Asignacion de valores al arreglo promedio
         }
         System.out.println("Reporte de notas:");
-        System.out.println("\t\tNota1   Nota2   Promedio");
-        for (int i = 1; i < promedios.length; i++) {
+        System.out.println("\t\tNota1   Nota2   Promedio"); // Titulos de presentacion
+        for (int i = 1; i < promedios.length; i++) { // Ciclo for para presentar
 
-            System.out.printf("Estudiante %d:\t%.2f\t%.2f\t%.2f\n", i, calificacion_1[i], calificacion_2[i], promedios[i]);
+            System.out.printf("Estudiante %d:\t%.2f\t%.2f\t%.2f\n", i, calificacion_1[i], calificacion_2[i], promedios[i]); // Presentación
         }
     }
 }
